b60c78264274b471c80d44e387b413334d9806b5
Stephen Koch <47271868+kochsj@users.noreply.github.com>
2019-12-04 21:29:01 -0800

    added writing functionality to a file called new_madlib. reformatted line breaks into the template.txt file

--- madlib.py
+++ madlib.py
@@ -1,24 +1,24 @@
 # ###################
 # Intro message
-# print('*'*51)
-# print('*'*51)
-# print('*'*5, ' '*39, '*'*5)
-# print('*'*5, ' '*8, '~ MADLIBS™ CLI GAME ~', ' '*8, '*'*5)
-# print('*'*5, ' '*39, '*'*5)
-# print('*'*5, ' '*39, '*'*5)
-# print('*'*5, ' '*4, 'Respond to the prompts below!', ' '*4, '*'*5)
-# print('*'*5, ' '*3, 'Prompts may ask you for a noun,', ' '*3, '*'*5)
-# print('*'*5, ' '*3, 'verb, pronoun, first name, etc.', ' '*3, '*'*5)
-# print('*'*5, ' '*39, '*'*5)
-# print('*'*5, ' '*2, 'MadLibs™ will use your responses,', ' '*2, '*'*5)
-# print('*'*5, ' '*4, 'to create a wacky cool story!', ' '*4, '*'*5)
-# print('*'*5, ' '*5, "Type 'start' to get started!", ' '*4, '*'*5)
-# print('*'*5, ' '*9, "Type 'quit' to exit.", ' '*8, '*'*5)
-# print('*'*5, ' '*39, '*'*5)
-# print('*'*51)
-# print('*'*51)
+print('*'*51)
+print('*'*51)
+print('*'*5, ' '*39, '*'*5)
+print('*'*5, ' '*8, '~ MADLIBS™ CLI GAME ~', ' '*8, '*'*5)
+print('*'*5, ' '*39, '*'*5)
+print('*'*5, ' '*39, '*'*5)
+print('*'*5, ' '*4, 'Respond to the prompts below!', ' '*4, '*'*5)
+print('*'*5, ' '*3, 'Prompts may ask you for a noun,', ' '*3, '*'*5)
+print('*'*5, ' '*3, 'verb, pronoun, first name, etc.', ' '*3, '*'*5)
+print('*'*5, ' '*39, '*'*5)
+print('*'*5, ' '*2, 'MadLibs™ will use your responses,', ' '*2, '*'*5)
+print('*'*5, ' '*4, 'to create a wacky cool story!', ' '*4, '*'*5)
+print('*'*5, ' '*5, "Type 'start' to get started!", ' '*4, '*'*5)
+print('*'*5, ' '*9, "Type 'quit' to exit.", ' '*8, '*'*5)
+print('*'*5, ' '*39, '*'*5)
+print('*'*51)
+print('*'*51, end="\n"*3)
 
-f = open("example.txt").read()
+f = open("template.txt").read()
 
 def create_list_of_inputs(file):
     list_of_prompts = [('', 0, -1)]
@@ -32,24 +32,32 @@ def create_list_of_inputs(file):
         idx += 1
     # print(list_of_prompts)
     return list_of_prompts
-create_list_of_inputs(f)
+# create_list_of_inputs(f)
+
 def construct_the_madlib(file):
     madlib = ''
     prompt_list = create_list_of_inputs(file)
     idx = 1
     while idx < (len(prompt_list)-1):
-        response = input('Enter ' + prompt_list[idx][0] + ':')
+        response = input('Enter ' + prompt_list[idx][0] + ': ')
         madlib += file[(prompt_list[idx-1][2]+1):prompt_list[idx][1]] + response
         idx += 1
     madlib += file[(prompt_list[idx-1][2]+1):]
-    print(madlib)
-construct_the_madlib(f)
+    new_madlib = open('new_madlib.txt', "w+")
+    new_madlib.write(madlib)
+    print('\n'*3)
+    print('*'*100)
+    print('*'*100, end="\n"*2)
+    print(madlib, end="\n"*2)
+    print('*'*100)
+    print('*'*100)
+# construct_the_madlib(f)
 
-# while True:
-#     response = input('...').lower()
-#     if response == 'quit':
-#         break
-#     if response == 'start':
-#         construct_the_madlib(f)
-#     else:
-#         print("Please type 'start' to get started. Or 'quit' to quit")
+while True:
+    response = input('--> ').lower()
+    if response == 'quit':
+        break
+    if response == 'start':
+        construct_the_madlib(f)
+    else:
+        print("Please type 'start' to get started. Or 'quit' to quit")
